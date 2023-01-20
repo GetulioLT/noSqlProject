9a51738b6d5d13fd2eab246067c1dfe6d47314cb
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-01-20 09:49:34 -0300

    0.0.6.3
    Adicionando comentários de funcionalidade no programa, e resolução de bugs, além de implementado a verificação de buracos nas matriculas.

--- Funcions.py
+++ Funcions.py
@@ -3,54 +3,113 @@ import json
 import requests
 
 
-#Função especifica para o cadastro de novo aluno
+# Função específica para o cadastro de novo aluno
 def novoAluno(matricula, link, chaveMat):
+    Alunos = requests.get(f"{link}/.json")
+
+    # Dois parametros para a verificação da matrícula atual
+    matriculaReal = 0
+    listaMatricula = list()
+
+    # Informações para o cadastramento do aluno
     nome = input("Digite o nome do aluno: ")
     idade = int(input("Digite a idade do aluno: "))
-    sala = int(input("Qual a sala do aluno:\n" +
-                 "1 - A\n" +
-                 "2 - B\n" +
-                 "3 - C\n"))
+    sala = int(input("Qual a sala do aluno(digite a numeração):\n"
+                     "1 - Classe A\n"
+                     "2 - Classe B\n"
+                     "3 - Classe C\n"))
 
-
+    # Laço que irá forçar uma das 3 opções de sala
     while True:
-        if sala in 123:
+        if sala in [1, 2, 3]:
             break
 
-        sala = input("Sala inexistente digite novamente: ")
+        sala = int(input("Qual a sala do aluno:\n"
+                         "1 - A\n"
+                         "2 - B\n"
+                         "3 - C\n"))
 
-    match sala.upper():
-        case "A":
+    # A parti da opção irá selecionar a sala adequada
+    match sala:
+        case 1:
             sala = "A"
 
-        case "B":
+        case 2:
             sala = "B"
 
-        case "C":
+        case 3:
             sala = "C"
 
+    # Conexão para verificar se as informações mencionadas já existem
     verificacao = requests.get(f"{link}/.json")
 
+    # Condisão é necessaria, pois só entrará se existir algum aluno previamente cadastrado
     if verificacao.text != 'null':
         for i in verificacao.json():
 
-            a = verificacao.json()[i]
+            info = verificacao.json()[i]
 
-            while nome == a["Nome"] and idade == a["Idade"] and sala == a["Sala"]:
+            while nome == info["Nome"] and idade == info["Idade"] and sala == info["Sala"]:
                 print("Aluno já cadastrado")
-                nome = input("Digite outro nome: ")
-                idade = int(input("Digite novamente a idade do aluno: "))
-                sala = input("Digite novamente a Sala: ")
+                nome = input("Digite o nome do aluno: ")
+                idade = int(input("Digite a idade do aluno: "))
+                sala = int(input("Qual a sala do aluno(digite a numeração):\n"
+                                 "1 - Classe A\n"
+                                 "2 - Classe B\n"
+                                 "3 - Classe C\n"))
 
-    Alunos = {"Nome": nome, "Idade": idade, "Matricula": matricula, "Sala": sala}
+                while True:
+                    if sala in [1, 2, 3]:
+                        break
 
-    requests.post(f"{link}/.json", data=json.dumps(Alunos))
+                    sala = int(input("Qual a sala do aluno:\n"
+                                     "1 - A\n"
+                                     "2 - B\n"
+                                     "3 - C\n"))
 
-    requests.patch(f"{chaveMat}/.json", data=json.dumps({"Matricula": matricula + 1}))
+                match sala:
+                    case 1:
+                        sala = "A"
+
+                    case 2:
+                        sala = "B"
+
+                    case 3:
+                        sala = "C"
+
+    """Verificação de buracos de matricula, onde não será possivel pular numeros de matriculas, não podendo
+    ficar 1, 3, 5."""
+    if matricula != 1:
+        # Adicionará todas as matrículas a uma lista
+        for i in Alunos.json():
+            listaMatricula.append(Alunos.json()[i]["Matricula"])
+
+        # Verificação se não há buracos na lista
+        for i in range(1, matricula + 1):
+            if i not in listaMatricula:
+                matriculaReal = i
+                break
+            # Se não houver a matrícula atual será inserida
+            else:
+                matriculaReal = matricula
+                matricula += 1
+    else:
+        matriculaReal = matricula
+
+    # Adicionando as infomações do aluno no dicionário
+    NovoAluno = {"Nome": nome, "Idade": idade, "Matricula": matriculaReal, "Sala": sala}
+
+    # Mandando as iformações do aluno para o banco de dados
+    requests.post(f"{link}/.json", data=json.dumps(NovoAluno))
+
+    # Atualização do número da matrícula atual
+    requests.patch(f"{chaveMat}/.json", data=json.dumps({"Matricula": matricula}))
+
+    listaMatricula.clear()
 
 
 def apagarAluno(link):
-    contador = 0
+    confirmacao = 0
 
     Alunos = requests.get(f"{link}/.json")
 
@@ -70,9 +129,9 @@ def apagarAluno(link):
         for j in Alunos.json()[i]:
             if matricula == Alunos.json()[i][j]:
                 requests.delete(f"{link}/{i}/.json")
-                contador += 1
+                confirmacao += 1
 
-    if contador == 0:
+    if confirmacao == 0:
         print("Aluno não encontrado")
 
 
@@ -112,7 +171,29 @@ def altAlunos(link):
             if matricula == Alunos.json()[i][j]:
                 nome = input("Digite o nome do aluno: ")
                 idade = int(input("Digite a idade do aluno: "))
-                sala = input("Qual a sala do aluno: ")
+                sala = int(input("Qual a sala do aluno(digite a numeração):\n"
+                                 "1 - Classe A\n"
+                                 "2 - Classe B\n"
+                                 "3 - Classe C\n"))
+
+                while True:
+                    if sala in [1, 2, 3]:
+                        break
+
+                    sala = int(input("Qual a sala do aluno:\n"
+                                     "1 - A\n"
+                                     "2 - B\n"
+                                     "3 - C\n"))
+
+                match sala:
+                    case 1:
+                        sala = "A"
+
+                    case 2:
+                        sala = "B"
+
+                    case 3:
+                        sala = "C"
 
                 newAluno = {"Nome": nome, "Idade": idade, "Matricula": matricula, "Sala": sala}
 
--- NoSql.py
+++ NoSql.py
@@ -1,79 +1,87 @@
-'''
+"""
 Pagina de código com todo o controle do banco de dados, tendo primeiramente a
 importação da pagina de código das funções.
-'''
+"""
 
-#Importação da pag funcions
+# Importação da pag. Funcions
 from Funcions import *
 
-#Link do bancos de dados
-link = "https://teste-in-getu-default-rtdb.firebaseio.com/"
+# Conexão do banco de dados
+link = "https://nosql-python-default-rtdb.firebaseio.com/"
 
-#link para pegar o valor atual de matricula
+# link para pegar o valor atual de matricula
 link_mat = link
 
-#Adicionado ao link a parte de alunos, para assim criar uma aba só pros alunos
+# Adicionado ao link a parte de alunos, para assim criar uma aba só pros alunos
 link += "Alunos"
 
-#Pegando o valor atual da matricula e atribuindo a uma variavel
+# Pegando o valor atual da matrícula e atribuindo a uma variavel
 mat = requests.get(f"{link_mat}.json")
 
-#Condição de verificação do sistema, caso não tenha nenhum dado iniciar a matricula em 1
+# Condição de verificação do sistema, caso não tenha nenhum dado iniciar a matrícula em 1
 if mat.text == "null":
     requests.post(f"{link_mat}.json",
                   data=json.dumps({"Matricula": 1}))
 
-#Requisitando a matricula novamente
+# Requisitando a matricula novamente
 mat = requests.get(f"{link_mat}.json")
 
-#Pegando o link da matricula, onde irá buscar qual a chave aleatoria atual
+# Pegando o link da matricula, onde irá buscar qual a chave aleatoria atual
 for i in mat.json():
     for j in mat.json()[i]:
         if j == "Matricula":
             chaveMat = f"{link_mat}{i}"
 
-#Pegando o numero da matricula
+# Pegando o número da matricula
 matricula = requests.get(f"{chaveMat}/.json").json()["Matricula"]
 
 print("Bem Vindo ao Sitema Escolar")
 
-#Laço idefinido que irá roda o sistem de modo a se repetir até digitar 0
+# Laço idefinido que irá roda o sistem de modo a se repetir até digitar 0
 while True:
-    escolha = int(input("Para prosseguir Digite uma das opções:\n" +
-                        "1 - Cadastrar novo Aluno\n" +
-                        "2 - Ver Todos os Alunos\n" +
-                        "3 - Deletar um aluno\n" +
-                        "4 - Resetar sistema\n" +
-                        "5 - Alterar informações\n" +
+    escolha = int(input("Para prosseguir Digite uma das opções:\n"
+                        "1 - Cadastrar novo Aluno\n"
+                        "2 - Ver Todos os Alunos\n"
+                        "3 - Deletar um aluno\n"
+                        "4 - Resetar sistema\n"
+                        "5 - Alterar informações\n"
                         "0 - Sair\n"))
 
-    #Irá analisar a escolha e a parti dela fazer uma ação logo em seguida
+    # Irá analisar a escolha e a parti dela fazer uma ação logo em seguida
     match escolha:
+        # Primeira opção irá fazer o cadastro de um novo aluno
         case 1:
             novoAluno(matricula, link, chaveMat)
             input("Matricula Feita, Aperte enter para continuar...")
             matricula += 1
 
+        # Irá Pegar todos os alunos até então cadastrados no sistema
         case 2:
             verAlunos(link)
             input("==========================================\n" +
                   "Aperte enter para continuar...")
 
+        # Irá deletar um aluno especifico
         case 3:
             apagarAluno(link)
             input("Aluno deletado com sucesso, Aperte enter para continuar...")
 
+        # Irá resetar o sistema geral
         case 4:
-            requests.delete("https://teste-in-getu-default-rtdb.firebaseio.com/.json")
+            requests.delete(f"{link_mat}.json")
             matricula = 1
             input("Sistema resetado com sucesso, Aperte enter para continuar...")
 
+        # Irá alterar as informações de um aluno especifico
         case 5:
             altAlunos(link)
+            input("Dados atualizados com sucesso, Aperte enter para continuar...")
 
+        # Finalizará o sistema
         case 0:
             break
 
+        # Quando for selecionado uma opção que não existe irá cair nesse case
         case _:
             print("Essa opção não existe")
             input("Aperte enter pra continuar...")
